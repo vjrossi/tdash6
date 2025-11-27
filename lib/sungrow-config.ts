58ea0ad783dc4f69de8ed7552c33339125f33269
tino.j.rossi <tino.j.rossi@gmail.com>
2025-11-27 12:17:55 +0000

    fix  SUNGROW_TOKEN_URL

--- lib/sungrow-config.ts
+++ lib/sungrow-config.ts
@@ -10,4 +10,4 @@ console.log('NEXT_PUBLIC_SUNGROW_REDIRECT_URL:', SUNGROW_REDIRECT_URL);
 export const SUNGROW_RSA_PUBLIC_KEY = process.env.SUNGROW_RSA_PUBLIC_KEY || '';
 console.log('SUNGROW_RSA_PUBLIC_KEY:', SUNGROW_RSA_PUBLIC_KEY);
 
-export const SUNGROW_TOKEN_URL = 'https://auweb3.isolarcloud.com/openapi/apiManage/token';
+export const SUNGROW_TOKEN_URL = 'https://augateway.isolarcloud.com/openapi/apiManage/token';
